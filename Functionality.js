b45d4eb36fdfe066ed9e0154974eced22ef31f08
Nechama Verbov <53469884+NechamaVerbov@users.noreply.github.com>
2019-12-12 12:53:00 +0200

    Photo gallery with different features: expand, delete, color, download image

---
+++ Functionality.js
@@ -0,0 +1,97 @@
+﻿let numPage = 1
+
+document.addEventListener("DOMContentLoaded", function (event)
+{
+   /*let tag = localStorage.getItem("tag");
+   document.getElementById("txt_tag").value = tag;*/
+   getPhotos();
+}, false)
+
+
+
+function populatePhotos(photoArr)
+{
+    let container=document.getElementById("image_gallery");
+    photoArr.map(
+        function (photo)
+        {
+            let img = new image(photo);
+            let elem = img.render();
+            container.appendChild(elem);
+        })
+}
+
+function getTag()
+{
+    let tag = document.getElementById("txt_tag").value
+    return tag;
+}
+
+function getImagesByTag()
+{
+    let val = getTag();
+    document.getElementById("image_gallery").innerHTML = "";
+    getPhotos();
+}
+
+function getPhotos()
+{
+    let val = getTag();
+    if (!val) { val = "sunrise" }
+    let container = document.getElementById("image_gallery");
+    if (document.getElementsByClassName("loading").length == 0)
+    {
+        let div = document.createElement("div")
+        div.className = "loading";
+        container.appendChild(div);
+    }
+    
+
+    /*localStorage.setItem("tag", val);*/
+    let url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=860dc345a325c43cd56de2229b5a35c7&tags=${val}&per_page=30&extras=url_l&page=${numPage}&format=json&nojsoncallback=1`;
+    fetch(url)
+        .then(function (response)
+        {
+            return response.json()
+            
+        }, function (response)
+        {
+            console.log("error")
+        })
+    .then(function (res)
+    {
+        populatePhotos(res.photos.photo);
+        document.getElementsByClassName("loading")[0].remove();
+    });
+}
+
+function checkKey(event) {
+    if (event.keyCode === 13) {
+        let val = getTag();
+        document.getElementById("image_gallery").innerHTML = "";
+        getPhotos();
+    }
+}
+
+function cancelLarge()
+{
+    let tmp = document.getElementById("enlarged_img");
+    tmp.classList.remove("expand_img");
+    tmp.className = "hide";
+}
+
+function morePics()
+{
+    let elm = document.getElementById("image_gallery");
+
+    
+    if (elm.scrollTop + elm.clientHeight == elm.scrollHeight)
+    {
+        numPage++;
+
+        getPhotos();
+
+        
+    }
+
+}
---
+++ ImageClass.js
@@ -0,0 +1,106 @@
+﻿class image
+    {
+        constructor(photo)
+        {
+            this.photo = photo;
+        }
+        buildUrl()
+        {
+            return `https://farm${this.photo.farm}.staticflickr.com/${this.photo.server}/${this.photo.id}_${this.photo.secret}.jpg`
+        }
+        render()
+        {
+            let img_src = this.buildUrl();
+            let div = document.createElement("div")
+            div.className = "pictureFrame";
+            div.style.backgroundImage = `url(${img_src})`;
+
+            let div2 = document.createElement("div");
+            div2.className = "buttons";
+
+            let btn1 = document.createElement("button");
+            btn1.className = "img_btn"; 
+            let i1 = document.createElement("i");
+            i1.className = "fa fa-expand fa-lg";
+            btn1.appendChild(i1);
+            btn1.onclick = () => {
+                this.enlarge(this.photo.url_l)
+            };
+
+            let btn2 = document.createElement("button");
+            btn2.className = "img_btn";
+            let i2 = document.createElement("i");
+            i2.className = "fa fa-trash fa-lg";
+            btn2.appendChild(i2);
+            btn2.onclick = ()=> {
+                this.removeElement(div)
+            };
+
+            let btn3 = document.createElement("button");
+            btn3.className = "img_btn";
+            let i3 = document.createElement("i");
+            i3.className = "fa fa-download fa-lg";
+            btn3.appendChild(i3);
+            btn3.onclick = ()=> {
+                this.downloadImg(img_src)
+            };
+
+            let btn4 = document.createElement("button");
+            btn4.className = "img_btn";
+            let i4 = document.createElement("i");
+            i4.className = "fa fa-adjust fa-lg";
+            btn4.appendChild(i4);
+   
+            btn4.onclick = ()=> {
+                this.invertImage(this.photo.url_l);
+            };
+
+            div2.appendChild(btn1);
+            div2.appendChild(btn2);
+            div2.appendChild(btn3);
+            div2.appendChild(btn4);
+
+            div.appendChild(div2);
+            return div;
+        }
+        removeElement(div)
+        {
+            div.remove();
+        }
+        enlarge(img_src)
+        {
+            let tmp = document.getElementById("enlarged_img");
+            tmp.classList.remove("hide");
+            tmp.className = "expand_img";
+            tmp.style.backgroundImage = `url(${img_src})`; 
+        }
+
+        downloadImg(urlImg) {
+            this.toDataURL(urlImg).then((obj_url) => {
+                let a = document.createElement('a');
+                a.href = obj_url;
+                a.download = "image.png";
+                document.body.appendChild(a);
+                a.click();
+                document.body.removeChild(a);
+            });
+
+        }
+
+        toDataURL(urlImg) {
+            return fetch(urlImg).then((response) => {
+                return response.blob();
+            }).then((blob) => {
+                return URL.createObjectURL(blob);
+            });
+        }
+
+        invertImage(img_src)
+        {
+            let tmp = document.getElementById("enlarged_img");
+            tmp.classList.remove("hide");
+            tmp.className = "expand_img";
+            tmp.style.backgroundImage = `url(${img_src})`;
+            tmp.classList.add("invert");
+        }
+    }
